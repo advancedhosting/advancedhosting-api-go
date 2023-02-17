220c0c3b237569dd8312d2379e578e8f5e595242
hrybun <max.hrybun@smartteksas.com>
2023-02-17 23:11:12 +0200

    Add tokens

---
+++ ah/tokens.go
@@ -0,0 +1,117 @@
+/*
+Copyright 2021 Advanced Hosting
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ah
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// Token object
+type Token struct {
+	ID        string   `json:"id,omitempty"`
+	Name      string   `json:"name,omitempty"`
+	Token     string   `json:"token,omitempty"`
+	Scopes    []string `json:"scopes,omitempty"`
+	ExpiresIn string   `json:"expires_in,omitempty"`
+	CreatedAt string   `json:"created_at,omitempty"`
+}
+
+// TokenCreateRequest object
+type TokenCreateRequest struct {
+	Name string `json:"name,omitempty"`
+}
+
+// TokensAPI is an interface for tokens.
+type TokensAPI interface {
+	List(context.Context, *ListOptions) ([]Token, error)
+	Get(context.Context, string) (*Token, error)
+	Create(context.Context, *TokenCreateRequest) (*Token, error)
+	Delete(context.Context, string) error
+}
+
+// TokensService implements TokensAPI interface.
+type TokensService struct {
+	client *APIClient
+}
+
+// tokensRoot is a root object for tokens
+type tokensRoot struct {
+	Token  *Token  `json:"token,omitempty"`
+	Tokens []Token `json:"tokens,omitempty"`
+}
+
+// Get returns a token by ID
+func (s *TokensService) Get(ctx context.Context, tokenId string) (*Token, error) {
+	path := fmt.Sprintf("id/api/v1/access_tokens/%s", tokenId)
+	req, err := s.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var tokensRoot tokensRoot
+	_, err = s.client.Do(ctx, req, &tokensRoot)
+	if err != nil {
+		return nil, err
+	}
+
+	return tokensRoot.Token, nil
+}
+
+// List returns all available tokens
+func (s *TokensService) List(ctx context.Context, options *ListOptions) ([]Token, error) {
+	path := "id/api/v1/access_tokens"
+
+	var tokensRoot tokensRoot
+	if err := s.client.list(ctx, path, options, &tokensRoot); err != nil {
+		return nil, err
+	}
+
+	return tokensRoot.Tokens, nil
+}
+
+// Create creates a new token
+func (s *TokensService) Create(ctx context.Context, request *TokenCreateRequest) (*Token, error) {
+	path := "id/api/v1/access_tokens"
+	req, err := s.client.newRequest(http.MethodPost, path, request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tokensRoot tokensRoot
+	if _, err := s.client.Do(ctx, req, &tokensRoot); err != nil {
+		return nil, err
+	}
+
+	return tokensRoot.Token, nil
+}
+
+// Delete deletes a token by ID
+func (s *TokensService) Delete(ctx context.Context, tokenId string) error {
+	path := fmt.Sprintf("id/api/v1/access_tokens/%s", tokenId)
+	req, err := s.client.newRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(ctx, req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ ah/tokens_test.go
@@ -0,0 +1,132 @@
+/*
+Copyright 2021 Advanced Hosting
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ah
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+const tokenResponse = `{
+    "id": "ded95980-05d8-44aa-977d-3dfc1e7966ba",
+    "name": "k8s-WKUB100000-token-ca96d",
+    "token": "test_token",
+    "scopes": ["email"],
+    "expires_in": "2021-12-15T17:51:29.765Z",
+    "created_at": "2021-12-15T17:51:29.765Z"
+}`
+
+var (
+	tokenGetResponse  = fmt.Sprintf(`{"token": %s}`, tokenResponse)
+	tokenListResponse = fmt.Sprintf(`{"tokens": [%s]}`, tokenResponse)
+)
+
+func TestTokensService_List(t *testing.T) {
+	fakeResponse := &fakeServerResponse{responseBody: tokenListResponse, statusCode: http.StatusOK}
+	server := newFakeServer("/id/api/v1/access_tokens", fakeResponse)
+	fakeClientOptions := &ClientOptions{
+		Token:      "test_token",
+		BaseURL:    server.URL,
+		HTTPClient: server.Client(),
+	}
+	api, _ := NewAPIClient(fakeClientOptions)
+
+	ctx := context.Background()
+	tokens, err := api.Tokens.List(ctx, nil)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	var expectedResult tokensRoot
+	if err = json.Unmarshal([]byte(tokenListResponse), &expectedResult); err != nil {
+		t.Errorf("Unexpected unmarshal error: %v", err)
+	}
+
+	if !reflect.DeepEqual(expectedResult.Tokens, tokens) {
+		t.Errorf("unexpected result, expected %v. got: %v", expectedResult, tokens)
+	}
+}
+
+func TestTokensService_Get(t *testing.T) {
+	fakeResponse := &fakeServerResponse{responseBody: tokenGetResponse, statusCode: http.StatusOK}
+	server := newFakeServer("/id/api/v1/access_tokens/ded95980-05d8-44aa-977d-3dfc1e7966ba", fakeResponse)
+	fakeClientOptions := &ClientOptions{
+		Token:      "test_token",
+		BaseURL:    server.URL,
+		HTTPClient: server.Client(),
+	}
+	api, _ := NewAPIClient(fakeClientOptions)
+
+	ctx := context.Background()
+	token, err := api.Tokens.Get(ctx, "ded95980-05d8-44aa-977d-3dfc1e7966ba")
+	if err != nil {
+		t.Errorf("Error getting token: %v", err)
+	}
+	var expectedResult tokensRoot
+	if err = json.Unmarshal([]byte(tokenGetResponse), &expectedResult); err != nil {
+		t.Errorf("Unexpected unmarshal error: %v", err)
+	}
+
+	if !reflect.DeepEqual(expectedResult.Token, token) {
+		t.Errorf("unexpected result, expected %v. got: %v", expectedResult.Token, token)
+	}
+}
+
+func TestTokensService_Create(t *testing.T) {
+	fakeResponse := &fakeServerResponse{responseBody: tokenGetResponse, statusCode: http.StatusOK}
+	server := newFakeServer("/id/api/v1/access_tokens", fakeResponse)
+	fakeClientOptions := &ClientOptions{
+		Token:      "test_token",
+		BaseURL:    server.URL,
+		HTTPClient: server.Client(),
+	}
+	api, _ := NewAPIClient(fakeClientOptions)
+
+	ctx := context.Background()
+	token, err := api.Tokens.Create(ctx, &TokenCreateRequest{Name: "test"})
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	var expectedResult tokensRoot
+	if err = json.Unmarshal([]byte(tokenGetResponse), &expectedResult); err != nil {
+		t.Errorf("Unexpected unmarshal error: %v", err)
+	}
+
+	if !reflect.DeepEqual(expectedResult.Token, token) {
+		t.Errorf("unexpected result, expected %v. got: %v", expectedResult, token)
+	}
+}
+
+func TestTokensService_Delete(t *testing.T) {
+	fakeResponse := &fakeServerResponse{responseBody: "", statusCode: http.StatusNoContent}
+	server := newFakeServer("/id/api/v1/access_tokens/ded95980-05d8-44aa-977d-3dfc1e7966ba", fakeResponse)
+	fakeClientOptions := &ClientOptions{
+		Token:      "test_token",
+		BaseURL:    server.URL,
+		HTTPClient: server.Client(),
+	}
+	api, _ := NewAPIClient(fakeClientOptions)
+
+	ctx := context.Background()
+	err := api.Tokens.Delete(ctx, "ded95980-05d8-44aa-977d-3dfc1e7966ba")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
